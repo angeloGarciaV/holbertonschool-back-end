ad7026215be23ad21eb6a8c4697a8e61b4ad7f1f
Angelo Garcia <angelogarcia1029@gmail.com>
2024-04-15 22:09:29 -0400

    requirement fixes

--- api/3-dictionary_of_list_of_dictionaries.py
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -3,28 +3,28 @@
 import json
 import requests
 
+if __name__ == "__main__":
+    EMPLOYEE_DATA = requests.get(
+        f'https://jsonplaceholder.typicode.com/users').json()
 
-EMPLOYEE_DATA = requests.get(
-    f'https://jsonplaceholder.typicode.com/users').json()
+    todo_DATA = requests.get(
+        f'https://jsonplaceholder.typicode.com/todos').json()
 
-todo_DATA = requests.get(
-    f'https://jsonplaceholder.typicode.com/todos').json()
+    USER_DATA = {}
 
-USER_DATA = {}
+    with open('todo_all_employees.json', 'w') as jsonfile:
+        for employees in range(0, len(EMPLOYEE_DATA)):
+            USER_ID = EMPLOYEE_DATA[employees].get('id')
+            USERNAME = EMPLOYEE_DATA[employees].get('username')
 
-with open('todo_all_employees.json', 'w') as jsonfile:
-    for employees in range(0, len(EMPLOYEE_DATA)):
-        USER_ID = EMPLOYEE_DATA[employees].get('id')
-        USERNAME = EMPLOYEE_DATA[employees].get('username')
+            USER_DATA[USER_ID] = []
 
-        USER_DATA[USER_ID] = []
+            for tasks in todo_DATA:
+                task_data = {
+                    "username": USERNAME,
+                    "task": tasks.get("title"),
+                    "completed": tasks.get("completed")
+                }
+                USER_DATA[USER_ID].append(task_data)
 
-        for tasks in todo_DATA:
-            task_data = {
-                "username": USERNAME,
-                "task": tasks.get("title"),
-                "completed": tasks.get("completed")
-            }
-            USER_DATA[USER_ID].append(task_data)
-
-    json.dump(USER_DATA, jsonfile)
+        json.dump(USER_DATA, jsonfile)
