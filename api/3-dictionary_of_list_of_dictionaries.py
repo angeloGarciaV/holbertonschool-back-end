33b39dc7cbb57c48be5f1f756f491b0f9f83708a
Angelo Garcia <angelogarcia1029@gmail.com>
2024-04-15 21:26:09 -0400

    Refactor API script to create JSON of all employees' tasks

--- api/3-dictionary_of_list_of_dictionaries.py
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-"""Module to create CSV"""
+"""Module to create JSON of all employees tasks"""
 import json
 import requests
-from sys import argv
 
 
 EMPLOYEE_DATA = requests.get(
@@ -11,21 +10,21 @@ EMPLOYEE_DATA = requests.get(
 todo_DATA = requests.get(
     f'https://jsonplaceholder.typicode.com/todos').json()
 
-
-EMPLOYEE_NAME = EMPLOYEE_DATA["name"]
-USER_ID = EMPLOYEE_DATA[int(user_id)-1]['id']
-USERNAME = EMPLOYEE_DATA[int(user_id)-1]['username']
-
+USER_DATA = {}
 
 with open('todo_all_employees.json', 'w') as jsonfile:
-    for employees in range(1, len(EMPLOYEE_DATA)):
-        USER_DATA = {USER_ID: []}
+    for employees in range(0, len(EMPLOYEE_DATA)):
+        USER_ID = EMPLOYEE_DATA[employees].get('id')
+        USERNAME = EMPLOYEE_DATA[employees].get('username')
+
+        USER_DATA[USER_ID] = []
 
         for tasks in todo_DATA:
             task_data = {
-                "task": tasks['title'],
-                "completed": tasks['completed'],
-                "username": USERNAME}
+                "username": USERNAME,
+                "task": tasks.get("title"),
+                "completed": tasks.get("completed")
+            }
             USER_DATA[USER_ID].append(task_data)
 
     json.dump(USER_DATA, jsonfile)
