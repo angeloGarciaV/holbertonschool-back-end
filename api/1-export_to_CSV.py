ad258c9fb7ed208d19f8cf2928bece18506206d0
Angelo Garcia <angelogarcia1029@gmail.com>
2024-04-15 22:02:21 -0400

    Refactor API script to improve code readability and efficiency

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -2,23 +2,23 @@
 """Module to create CSV"""
 import csv
 import requests
-from sys import argv
+import sys
 
-user_id = argv[1]
-EMPLOYEE_DATA = requests.get(
-    f'https://jsonplaceholder.typicode.com/users?Id={user_id}').json()
+if __name__ == "__main__":
+    user_id = sys.argv[1]
+    EMPLOYEE_DATA = requests.get(
+        f'https://jsonplaceholder.typicode.com/users?Id={user_id}').json()
 
-todo_DATA = requests.get(
-    f'https://jsonplaceholder.typicode.com/todos?userId={user_id}').json()
+    todo_DATA = requests.get(
+        f'https://jsonplaceholder.typicode.com/todos?userId={user_id}').json()
 
-EMPLOYEE_NAME = EMPLOYEE_DATA[int(user_id)-1]["name"]
+    EMPLOYEE_NAME = EMPLOYEE_DATA[int(user_id)-1].get("name")
 
-USER_ID = EMPLOYEE_DATA[int(user_id)-1]['id']
-USERNAME = EMPLOYEE_DATA[int(user_id)-1]['username']
+    USER_ID = EMPLOYEE_DATA[int(user_id)-1].get("id")
+    USERNAME = EMPLOYEE_DATA[int(user_id)-1].get("username")
 
-
-with open(f'{user_id}.csv', 'w', newline='') as csvfile:
-    writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
-    for tasks in todo_DATA:
-        writer.writerow(
-            [USER_ID, USERNAME, tasks['completed'], tasks['title']])
+    with open(f'{user_id}.csv', 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+        for tasks in todo_DATA:
+            writer.writerow(
+                [USER_ID, USERNAME, tasks.get('completed'), tasks.get('title')])
