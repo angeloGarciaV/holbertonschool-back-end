69289f2788350dc70e6a2c6a1aefa393a0ff57d2
Angelo Garcia <angelogarcia1029@gmail.com>
2024-04-15 22:08:27 -0400

    requirement fixes

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -2,30 +2,30 @@
 """Module to create CSV"""
 import json
 import requests
-from sys import argv
+import sys
 
-user_id = argv[1]
-user_num = int(user_id)
+if __name__ == "__main__":
+    user_id = sys.argv[1]
+    user_num = int(user_id)
 
-EMPLOYEE_DATA = requests.get(
-    f'https://jsonplaceholder.typicode.com/users?Id={user_id}').json()
+    EMPLOYEE_DATA = requests.get(
+        f'https://jsonplaceholder.typicode.com/users?Id={user_id}').json()
 
-todo_DATA = requests.get(
-    f'https://jsonplaceholder.typicode.com/todos?userId={user_id}').json()
+    todo_DATA = requests.get(
+        f'https://jsonplaceholder.typicode.com/todos?userId={user_id}').json()
 
+    EMPLOYEE_NAME = EMPLOYEE_DATA[int(user_id)-1].get("name")
+    USER_ID = EMPLOYEE_DATA[int(user_id)-1].get("id")
+    USERNAME = EMPLOYEE_DATA[int(user_id)-1].get("username")
 
-EMPLOYEE_NAME = EMPLOYEE_DATA[int(user_id)-1]["name"]
-USER_ID = EMPLOYEE_DATA[int(user_id)-1]['id']
-USERNAME = EMPLOYEE_DATA[int(user_id)-1]['username']
+    with open(f'{user_id}.json', 'w') as jsonfile:
+        USER_DATA = {USER_ID: []}
 
-with open(f'{user_id}.json', 'w') as jsonfile:
-    USER_DATA = {USER_ID: []}
+        for tasks in todo_DATA:
+            task_data = {
+                "task": tasks.get('title'),
+                "completed": tasks.get('completed'),
+                "username": USERNAME}
+            USER_DATA[USER_ID].append(task_data)
 
-    for tasks in todo_DATA:
-        task_data = {
-            "task": tasks['title'],
-            "completed": tasks['completed'],
-            "username": USERNAME}
-        USER_DATA[USER_ID].append(task_data)
-
-    json.dump(USER_DATA, jsonfile)
+        json.dump(USER_DATA, jsonfile)
